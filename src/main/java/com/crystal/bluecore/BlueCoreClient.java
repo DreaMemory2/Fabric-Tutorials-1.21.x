926cf4b2d9ea3f0b35a49ada64d072b8645612f0
DreaMemory2 <crystalstalight699@gmail.com>
2024-08-18 14:28:44 +0800

    updating

---
+++ src/main/java/com/crystal/bluecore/BlueCoreClient.java
@@ -0,0 +1,13 @@
+package com.crystal.bluecore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlueCoreClient implements ClientModInitializer {
+    /**
+     * 在客户端环境中运行模块初始化程序。
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/crystal/bluecore/BlueCoreDataGenerator.java
@@ -0,0 +1,11 @@
+package com.crystal.bluecore;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BlueCoreDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
