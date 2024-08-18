6b8a86cb861cf4a8174390f69b4a9d560a7a694f
DreaMemory2 <crystalstalight699@gmail.com>
2024-08-18 10:25:13 +0800

    updating

---
+++ src/main/java/com/crystal/bluecore/BlueCore.java
@@ -0,0 +1,17 @@
+package com.crystal.bluecore;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Very important comment
+public class BlueCore implements ModInitializer {
+
+	public static final String MOD_ID = "bluecore";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/crystal/bluecore/BlueCoreClient.java
@@ -0,0 +1,14 @@
+package com.crystal.bluecore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlueCoreClient implements ClientModInitializer {
+
+    /**
+     * 在客户端环境中运行模组初始化器。
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
